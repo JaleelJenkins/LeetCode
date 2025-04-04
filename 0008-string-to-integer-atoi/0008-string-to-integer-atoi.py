52cf325c913e0a0dd87af1be365084f38f1d8a9e
Jaleel Jenkins <86581823+JaleelJenkins@users.noreply.github.com>
2025-04-04 03:46:36 -0700

    Time: 1 ms (53.16%), Space: 18 MB (8.53%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,32 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+    
+        if s=="":
+            return 0
+
+
+        sign=1
+        if s[0]=='-':
+            sign = -1
+            s = s[1:]  
+        elif s[0]=='+':
+            s = s[1:] 
+        
+        val = 0
+        for i in s:
+            if i.isdigit(): 
+                val = val * 10 + int(i)
+            else:
+                break  
+
+        val *= sign
+        int_max = 2**31 - 1
+        int_min = -2**31
+        if val > int_max:
+            return int_max
+        elif val < int_min:
+            return int_min
+        
+        return val
+            
